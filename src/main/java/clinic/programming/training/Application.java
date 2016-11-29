65bd9c44fc8eb6921e42b142cd324ddd6607e5e9
Nandhakumar <kumarvvp7@gmail.com>
2016-11-29 22:49:34 +0530

    Modified POM

--- src/main/java/clinic/programming/training/Application.java
+++ src/main/java/clinic/programming/training/Application.java
@@ -1,7 +1,15 @@
 package clinic.programming.training;
-
+import java.util.ArrayList;
+import java.util.List;
 public class Application {
     
+	public void greet(){
+		List<String> mylist=new ArrayList<>();
+		mylist.add("sdf");
+		for(String myString:mylist){
+			System.out.println(myString);
+		}
+	}
     public Application() {
         System.out.println ("Inside Application");
     }
@@ -10,5 +18,6 @@ public class Application {
     public static void main (String[] args) {
     	System.out.println ("Starting Application");
 	Application app = new Application();
+	app.greet();
     }
 }
